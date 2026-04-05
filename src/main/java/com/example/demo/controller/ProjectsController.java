47061c2e475e0a04092fcb251fe6c955a28fe114
jebeen <jebeen@testmail.test>
2026-04-05 07:24:54 +0530

    "First commit - Maven project"

---
+++ src/main/java/com/example/demo/controller/ProjectsController.java
@@ -0,0 +1,100 @@
+package com.example.demo.controller;
+
+import com.example.demo.models.Projects;
+import com.example.demo.repository.ProjectsRepository;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.*;
+
+@RequestMapping("api/projects")
+@CrossOrigin(origins = "http://localhost:3000/", maxAge = 3600)
+@RestController
+public class ProjectsController {
+
+    
+    private final ProjectsRepository project;
+    // Connect the repository to the controller
+   
+
+    public ProjectsController(ProjectsRepository Repository) {
+        this.project = Repository;
+    }
+
+    @GetMapping
+    // public Iterable<Projects> getAllProjects() {
+    //     return this.project.findAll();
+    // }
+
+    // public List<Projects> getAllUsers() {
+    //     // CrudRepository.findAll() returns Iterable, convert it to a List if needed
+    //     return (List<Projects>) project.findAll();
+    // }
+ public ResponseEntity<?> getAll() {
+		List<Projects> result = (List<Projects>) project.findAll();
+		return new ResponseEntity(result, HttpStatus.OK);
+	}
+
+    // DELETE
+    @DeleteMapping(path = "/delete/{id}")
+    public ResponseEntity<?> deleteProject(@PathVariable Integer id) {
+        Optional<Projects> del = this.project.findById(id);
+        
+        if (del.isPresent()) {
+            this.project.delete(del.get());
+            System.out.println("Item"+id);
+            List<Projects> result = (List<Projects>) project.findAll();
+		    return new ResponseEntity(result, HttpStatus.OK);
+        } else {
+            List<Projects> result = (List<Projects>) project.findAll();
+            return new ResponseEntity(result, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // UPDATE
+    @PutMapping(path = "/update/{id}")
+    public ResponseEntity<?> projectUpdate(@RequestBody Projects proj, @PathVariable Integer id) {
+        Optional<Projects> UpdateOptional = this.project.findById(id);
+        if (UpdateOptional.isPresent()) {
+            Projects p = UpdateOptional.get();
+            if (proj.getDescription() != null) {
+                p.setDescription(proj.getDescription());
+            }
+            if (proj.getNo_of_months() != null) {
+                p.setNo_of_months(proj.getNo_of_months());
+            }
+            if (proj.getNo_of_years() != null) {
+                p.setNo_of_years(proj.getNo_of_years());
+            }
+            if (proj.getProjectname() != null) {
+                p.setProjectname(proj.getProjectname());
+            }
+            this.project.save(p);
+            List<Projects> result = (List<Projects>) project.findAll();
+            return new ResponseEntity(result, HttpStatus.OK);
+        } else {
+            List<Projects> result = (List<Projects>) project.findAll();
+            return new ResponseEntity(result, HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+    // READ
+    @GetMapping(path = "/{id}")
+    public Optional<Projects> getProjectById(@PathVariable Integer id) {
+        return this.project.findById(id);
+    }
+
+    // CREATE
+    @PostMapping(path = "/add")
+    public Projects addProject(@RequestBody Projects p) {
+        return this.project.save(p);
+    }
+
+    
+    
+}
---
+++ src/main/java/com/example/demo/models/Projects.java
@@ -0,0 +1,100 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="projects")
+public class Projects {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="no_of_years")
+    private Integer no_of_years;
+
+    @Column(name="no_of_months")
+    private Integer no_of_months;
+
+    @Column(name = "teamsize")
+    private Integer teamsize;
+
+    @Column(name = "projectname")
+    private String projectname;
+
+    @Column(name = "roletitle")
+    private String roletitle;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Integer getNo_of_years() {
+        return no_of_years;
+    }
+
+    public Integer getNo_of_months() {
+        return no_of_months;
+    }
+
+    public Integer getTeamsize() {
+        return teamsize;
+    }
+
+    public String getProjectname() {
+        return projectname;
+    }
+
+    public String getRoletitle() {
+        return roletitle;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setNo_of_years(Integer no_of_years) {
+        this.no_of_years = no_of_years;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + id + '\'' +
+                ", lastName='" + description + '\'' +
+                ", age=" + projectname +
+                '}';
+    }
+
+
+    public void setNo_of_months(Integer no_of_months) {
+        this.no_of_months = no_of_months;
+    }
+
+    public void setTeamsize(Integer teamsize) {
+        this.teamsize = teamsize;
+    }
+
+    public void setProjectname(String projectname) {
+        this.projectname = projectname;
+    }
+
+    public void setRoletitle(String roletitle) {
+        this.roletitle = roletitle;
+    }
+
+    
+
+
+}
